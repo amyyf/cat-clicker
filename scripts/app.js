f7fa675c95f92826f38a4b23cf1ac0622c18bc5f
Amy Frieson <ava447@gmail.com>
2018-07-24 09:56:28 -0400

    Enable click function for new Cat class

--- scripts/app.js
+++ scripts/app.js
@@ -2,30 +2,29 @@ const Cat = function (name, photo, description) {
   this.name = name;
   this.photo = photo;
   this.clicks = 0;
-  this.description = '';
+  this.description = description;
 };
 
-Cat.prototype.countClicks = function () {
-  this.photo.addEventListener('click', function () {
-    let count = this.clicks;
-    count++;
-  });
-};
-
-const rey = new Cat('Rey', 'img/calicocat.png', 'calico');
-const ron = new Cat('Ron', 'img/tabbycat.png', 'tabby');
-
-const generateCat = function (cat) {
+Cat.prototype.generateCat = function () {
   const section = document.createElement('section');
   const catArea = document.getElementById('cat-area');
   section.classList.add('cat');
   section.innerHTML = `
-    <h3>${cat.name}</h3>
-    <p>Number of clicks: <span id="click-counter">${cat.clicks}</span></p>
-    <img src='${cat.photo}' alt=${cat.description} cat' id=${cat.description}-cat' />
+    <h3>${this.name}</h3>
+    <p>Number of clicks: <span id="${this.name}-click-counter">${this.clicks}</span></p>
+    <img src='${this.photo}' alt='${this.description} cat' id='${this.description}-cat' />
   `;
   catArea.appendChild(section);
+  section.addEventListener('click', this.countClicks.bind(this));
 };
 
-generateCat(rey);
-generateCat(ron);
+Cat.prototype.countClicks = function () {
+  const clickCounter = document.getElementById(`${this.name}-click-counter`);
+  let count = this.clicks;
+  count++;
+  this.clicks = count;
+  clickCounter.textContent = count;
+};
+
+const rey = new Cat('Rey', 'img/calicocat.png', 'calico').generateCat();
+const ron = new Cat('Ron', 'img/tabbycat.png', 'tabby').generateCat();
