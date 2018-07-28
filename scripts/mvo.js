7ce9ac438463cfc914da68f76ae781299c7152ea
Amy Frieson <ava447@gmail.com>
2018-07-28 19:02:14 -0400

    Swap out cats when names are clicked

--- scripts/mvo.js
+++ scripts/mvo.js
@@ -1,30 +1,22 @@
 (function() {
   const model = {
-    // add a cat from octopus
     catList: [],
-    addCat: function (cat) {
-      this.catList.push(cat);
-    },
-    // give a cat to octopus
-    getCat: function (catId) {
-      return this.catList[catId];
-    },
-    // give all cats to octopus
-    init: function () {
-      console.log('model initialized');
+    /* receives catArray from octopus, populates catList data structure as an array of objects, and returns it to octopus for further use */
+    init: function (catArray) {
+      for (let cat of catArray) {
+        this.catList.push({
+          name: cat[0],
+          photo: cat[1],
+          description: cat[2],
+          clicks: 0
+        });
+      }
+      return this.catList;
     }
   }
 
   const octopus = {
-    // create new cat and give to model
-    newCat: function (name, photo, description) {
-      model.addCat({name: name,
-        photo: photo,
-        description: description,
-        clicks: 0
-      });
-    },
-    // countClicks function gives clicks to model and to view
+    // this function is called by the view when it renders a cat
     countClicks: function (cat) {
       let count = cat.clicks;
       count++;
@@ -32,46 +24,61 @@
       view.updateClicks(count);
     },
 
-    // gives cat data to view - catlist
-
-    // kicks off view and model
+    // initializes the model
     init: function () {
-      model.init();
-      view.init();
+      const catList = model.init([
+        ['Rey', 'img/calicocat.png', 'calico'],
+        ['Ron', 'img/tabbycat.png', 'tabby'],
+        ['Tigger', 'img/tigercat.png', 'tiger'],
+        ['Simon', 'img/persiancat.png', 'persian'],
+        ['Scratchy', 'img/tuxedocat.png', 'tuxedo']
+      ]);
+      // initializes the view with data from the model - the first cat displays as default
+      view.init(catList[0], catList);
     }
   }
 
   const view = {
-    // render default cat
-    init: function () {
+    // render list of cat names and default cat
+    init: function (firstCat, catList) {
       this.catArea = document.getElementById('cat-area');
-      console.log('view initialized');
+      this.listArea = document.getElementById('cat-list');
+      this.renderCatList(catList);
+      this.renderCat(firstCat);
     },
-    // render catlist
-
+    // render catlist on page
+    renderCatList: function (list) {
+      for (let cat of list) {
+        const li = document.createElement('li');
+        li.textContent = cat.name;
+        li.addEventListener('click',
+          this.renderCat.bind(this, cat) // passes in the view as value of 'this' and the specific cat as the arg
+        );
+        this.listArea.appendChild(li);
+      }
+    },
 
     // render selected cat
     renderCat: function (cat) {
-      const section = document.createElement('section');
-      section.classList.add('cat');
-      section.innerHTML = `
+      this.catArea.innerHTML = `
         <h3>${cat.name}</h3>
         <p>Number of clicks: <span id="click-counter">${cat.clicks}</span></p>
         <img src='${cat.photo}' alt='${cat.description} cat' id='${cat.description}-cat' />
       `;
-      this.catArea.appendChild(section);
-      this.catArea.addEventListener('click', function (e) {
-        octopus.countClicks(cat);
-      });
+      // remove previous cat's event listener
+      this.catArea.removeEventListener('click', this.activeListener);
+      this.activeListener = function (e) { // activeListener is a method created on the view
+        octopus.countClicks(cat); // octopus is what actually handles the counting
+      }
+      // add event listener to new cat
+      this.catArea.addEventListener('click', this.activeListener);
     },
+
     updateClicks: function (count) {
         const clickCounter = document.getElementById('click-counter');
         clickCounter.textContent = count;
     }
-
   }
-  octopus.init();
 
-  octopus.newCat('Rey', 'img/calicocat.png', 'calico');
-  view.renderCat(model.catList[0])
+  octopus.init();
 })();
