0931ec6bffd600f63073c8457d34573629b00911
Amy Frieson <ava447@gmail.com>
2018-07-28 17:06:34 -0400

    Render cat on screen

--- scripts/mvo.js
+++ scripts/mvo.js
@@ -25,6 +25,9 @@
       });
     },
     // countClicks function gives clicks to model and to view
+    countClicks: function () {
+      console.log('clicked');
+    },
 
     // gives cat data to view - catlist
 
@@ -43,8 +46,21 @@
     // select cat through catlist
 
     // render selected cat
+    render: function (cat) {
+      const catArea = document.getElementById('cat-area');
+      const section = document.createElement('section');
+      section.classList.add('cat');
+      section.innerHTML = `
+        <h3>${cat.name}</h3>
+        <p>Number of clicks: <span id="${cat.name}-click-counter">${cat.clicks}</span></p>
+        <img src='${cat.photo}' alt='${cat.description} cat' id='${cat.description}-cat' />
+      `;
+      catArea.appendChild(section);
+      catArea.addEventListener('click', octopus.countClicks.bind(this));
+    }
   }
 
   octopus.newCat('Rey', 'img/calicocat.png', 'calico');
+  view.render(model.catList[0])
   octopus.init();
 })();
