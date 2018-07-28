0f767be302eadb43e68ac5081706fb52fab564d6
Amy Frieson <ava447@gmail.com>
2018-07-28 13:35:11 -0400

    Sketch out restructuring plan

--- scripts/mvo.js
+++ scripts/mvo.js
@@ -1,14 +1,37 @@
 (function() {
   const model = {
-    
+    // add a cat from octopus
+
+    // give a cat to octopus
+
+    // give all cats to octopus
+    init: function () {
+
+    }
   }
 
   const octopus = {
+    // create new cat and give to model
 
+    // countClicks function gives clicks to model and to view
+
+    // gives cat data to view - catlist
+
+    // kicks off view and model
+    init: function () {
+      model.init();
+      view.init();
+    }
   }
 
   const view = {
+    // render default cat
+    init: function () {
 
+    }
+    // select cat through catlist
+
+    // render selected cat
   }
-
+  octopus.init();
 })();
